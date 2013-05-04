0512e8783dc21bec6d1d35b9fbf51188b6d15562
Erik Engheim <erik.engheim@mac.com>
2013-05-04 12:32:00 +0200

    General entity  object for reuse

---
+++ movableentity.go
@@ -0,0 +1,44 @@
+// Copyright 2012 Erik Engheim. All rights reserved.
+// Use of this source code is governed by a GPL-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	. "bensin/component"
+	. "bensin/graphics"
+	. "geom2d"
+	. "bensin/base"
+)
+
+type movableBehavior struct {
+	transform *Transform
+	body *Body
+}
+
+func NewMovableEntity(pos Point, poly Polygon) *Entity {
+	movable := NewEntity(pos)
+	behavior := new(movableBehavior)
+	behavior.transform = &movable.Transform
+	movable.Behavior = behavior
+	
+
+	visual := NewShapeVisual(poly)
+	visual.Color = Color{0, 1, 0, 0.5}
+	movable.Visual = visual
+	
+	var body Body
+	body.Speed = 1
+	body.Orientation = Rad(180)
+	body.Position = Point{3, 3}
+	behavior.body = &body
+	
+	return movable
+}
+
+
+func (mb *movableBehavior) Update(t, dt float64) {
+	mb.body.Update(t, dt)
+	mb.transform.Input(mb.body.Output())
+	mb.transform.Update(t, dt)
+}
--- player.go
+++ player.go
@@ -5,42 +5,17 @@
 package main
 
 import (
-	. "bensin/component"
-	. "bensin/graphics"
 	. "geom2d"
 	. "bensin/base"
 )
 
-type playerBehavior struct {
-	transform *Transform
-	body *Body
-}
 
 func NewPlayer(pos Point) *Entity {
-	player := NewEntity(pos)
-	behavior := new(playerBehavior)
-	behavior.transform = &player.Transform
-	player.Behavior = behavior
-	
 	poly := Polygon{Point{-2, -1},
 					Point{2, 0},
 					Point{-2, 1}}
-	visual := NewShapeVisual(poly)
-	visual.Color = Color{0, 1, 0, 0.5}
-	player.Visual = visual
-	
-	var body Body
-	body.Speed = 1
-	body.Orientation = Rad(180)
-	body.Position = Point{3, 3}
-	behavior.body = &body
+	player := NewMovableEntity(pos, poly)
 	
 	return player
 }
 
-
-func (pb *playerBehavior) Update(t, dt float64) {
-	pb.body.Update(t, dt)
-	pb.transform.Input(pb.body.Output())
-	pb.transform.Update(t, dt)
-}
